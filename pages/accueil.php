d3746ea3d70ac06f912fe694eba27d7b896fb242
Nicolas Savoie <nic-savoie@hotmail.fr>
2023-11-15 09:38:28 -0500

    ajout bouton panier

--- pages/accueil.php
+++ pages/accueil.php
@@ -8,7 +8,13 @@
 </head>
 <body>
     <header>
-        <h1>Teccart Wear</h1>
+        <div class="header-content">
+            <h1>Teccart Wear</h1>
+            <div class="cart">
+                <span id="cart-count">0</span>
+                <img src="../images/cart-icon.png" alt="Shopping Cart" id="cart-icon">
+            </div>
+        </div>
     </header>
     <nav>
         <ul>
@@ -23,27 +29,41 @@
         </div>
     </nav>
     <div class="container">
-        <div class="product">
+        <div class="product" onclick="addToCart('Chemise Élégante', 29.99)">
             <img src="../images/chemise.jpg" alt="Elegant Blue Dress Shirt">
             <h3>Chemise Élégante</h3>
             <p>Prix : $29.99</p>
             <a href="#">Acheter</a>
         </div>
-        <div class="product">
+        <div class="product" onclick="addToCart('Robe d\'Été', 39.99)">
             <img src="../images/robe.jpg" alt="Casual Summer Dress">
             <h3>Robe d'Été</h3>
             <p>Prix : $39.99</p>
             <a href="#">Acheter</a>
         </div>
-        <div class="product">
+        <div class="product" onclick="addToCart('Jeans Classiques', 49.99)">
             <img src="../images/jeans.jpg" alt="Classic Denim Jeans">
             <h3>Jeans Classiques</h3>
             <p>Prix : $49.99</p>
             <a href="#">Acheter</a>
         </div>
+        <!-- Add more products here if needed -->
     </div>
     <footer>
         <p>&copy; 2023 Teccart Wear. Tous droits réservés.</p>
     </footer>
+    <script>
+        let cartCount = 0;
+
+        function addToCart(productName, price) {
+            cartCount++;
+            updateCartCount(cartCount);
+            console.log(`Added ${productName} to the cart. Price: $${price}`);
+        }
+
+        function updateCartCount(count) {
+            document.getElementById('cart-count').innerText = count;
+        }
+    </script>
 </body>
 </html>
