d947632ca89401ea9ae68b1ca25b52ca0ac28199
Nicolas Savoie <nic-savoie@hotmail.fr>
2023-11-15 09:05:06 -0500

    gestion de structure

--- pages/accueil.php
+++ pages/accueil.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teccart Wear - Accueil</title>
-    <link rel="stylesheet" href="accueil.css">
+    <link rel="stylesheet" href="../css/accueil.css">
 </head>
 <body>
     <header>
@@ -19,31 +19,28 @@
             <li><a href="#">Contact</a></li>
         </ul>
         <div class="login-button">
-            <a href="login.php">Connexion</a>
+            <a href="../pages/login.php">Connexion</a>
         </div>
     </nav>
     <div class="container">
         <div class="product">
-            <img src="chemise.jpg" alt="Elegant Blue Dress Shirt">
+            <img src="../images/chemise.jpg" alt="Elegant Blue Dress Shirt">
             <h3>Chemise Élégante</h3>
             <p>Prix : $29.99</p>
             <a href="#">Acheter</a>
         </div>
         <div class="product">
-            <img src="robe.jpg" alt="Casual Summer Dress">
+            <img src="../images/robe.jpg" alt="Casual Summer Dress">
             <h3>Robe d'Été</h3>
             <p>Prix : $39.99</p>
             <a href="#">Acheter</a>
         </div>
         <div class="product">
-            <img src="jeans.jpg" alt="Classic Denim Jeans">
+            <img src="../images/jeans.jpg" alt="Classic Denim Jeans">
             <h3>Jeans Classiques</h3>
             <p>Prix : $49.99</p>
             <a href="#">Acheter</a>
         </div>
-
-   
-
     </div>
     <footer>
         <p>&copy; 2023 Teccart Wear. Tous droits réservés.</p>
