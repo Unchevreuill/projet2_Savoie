a7bce7113e3f2ecc3c1b538dc642721efde7831d
Nicolas Savoie <nic-savoie@hotmail.fr>
2023-12-12 15:29:27 -0500

    ajout information du user sur la page home

--- views/pages/home.php
+++ views/pages/home.php
@@ -13,23 +13,19 @@ if ($pdo) {
     // Récupérer les trois derniers produits de la base de données
     $query = $pdo->query("SELECT * FROM product ORDER BY id DESC LIMIT 3");
 
-    // Vérifier si la requête a réussi
     if ($query) {
         $latestProducts = $query->fetchAll(PDO::FETCH_ASSOC);
     } else {
-        // Gérer l'erreur de la requête SQL
         echo "Erreur de requête SQL";
         exit;
     }
 } else {
-    // Gérer l'erreur de la connexion à la base de données
     echo "Erreur de connexion à la base de données";
     exit;
 }
 
 // Vérifier si la variable de session 'panier' n'existe pas encore
 if (!isset($_SESSION['panier']) || !is_array($_SESSION['panier'])) {
-    // Initialiser le panier comme un tableau vide
     $_SESSION['panier'] = [];
 }
 
@@ -42,67 +38,64 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_to_cart'])) {
     $total = 0;
 
     // Vérifiez si l'utilisateur est connecté et obtenez son ID depuis la session
-    if (isset($_SESSION['user_id'])) {
-        $userId = $_SESSION['user_id'];
-    } else {
-        // Utilisateur non connecté, définissez l'ID par défaut à 1
-        $userId = 1;
-    }
+    $userId = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : 1;
 
     // Créez une instance de la classe HomeModel
     $homeModel = new \projet2_Savoie\Models\HomeModel($pdo);
 
-
     // Insérez la commande dans la table user_order
     $insertOrderQuery = $pdo->prepare("INSERT INTO user_order (ref, order_date, total, user_id) VALUES (:ref, :order_date, :total, :user_id)");
-    $ref = $homeModel->generateOrderReference(); // Utilisez la fonction generateOrderReference de HomeModel
+    $ref = $homeModel->generateOrderReference(); 
     $insertOrderQuery->bindParam(':ref', $ref);
     $insertOrderQuery->bindParam(':order_date', $orderDate);
     $insertOrderQuery->bindParam(':total', $total);
     $insertOrderQuery->bindParam(':user_id', $userId);
 
     if ($insertOrderQuery->execute()) {
-        // Récupérez l'ID de la commande nouvellement créée
         $orderId = $pdo->lastInsertId();
 
         // Ajoutez le produit au panier dans la table order_has_product
         $insertProductQuery = $pdo->prepare("INSERT INTO order_has_product (user_order_id, product_id, qtty, price) VALUES (:user_order_id, :product_id, :qtty, :price)");
         $insertProductQuery->bindParam(':user_order_id', $orderId);
         $insertProductQuery->bindParam(':product_id', $productId);
-        $quantity = 1; // Vous pouvez ajuster la quantité en fonction de votre logique
-        $price = $homeModel->getProductPrice($productId); // Utilisez la fonction getProductPrice de HomeModel
+        $quantity = 1; 
+        $price = $homeModel->getProductPrice($productId); 
         $insertProductQuery->bindParam(':qtty', $quantity);
         $insertProductQuery->bindParam(':price', $price);
 
         if ($insertProductQuery->execute()) {
-            // Rafraîchir la page pour refléter les changements
             header('Location: home.php');
             exit();
         } else {
-            // Gérer l'erreur d'insertion dans la table order_has_product
             echo "Erreur lors de l'ajout au panier";
         }
     } else {
-        // Gérer l'erreur d'insertion dans la table user_order
         echo "Erreur lors de la création de la commande";
     }
+}
+
+// Afficher un message de bienvenue si l'utilisateur est connecté
+$welcomeMessage = "Bienvenue visiteur !";
+if (isset($_SESSION['user'])) {
+    $welcomeMessage = "Bonjour " . htmlspecialchars($_SESSION['user']['fname']) . " " . htmlspecialchars($_SESSION['user']['lname']) . " !";
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teccart Wear - Accueil</title>
     <link rel="stylesheet" href="../../css/home.css">
 </head>
-
 <body>
     <header>
         <div class="header-content">
             <h1>Teccart Wear</h1>
+            <div class="welcome-message">
+                <?php echo $welcomeMessage; ?>
+            </div>
             <div class="cart">
                 <a href="panier.php">
                     <img id="cart-icon" src="../../images/cart-icon.png" alt="Panier">
@@ -122,13 +115,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_to_cart'])) {
     <main>
         <div class="new-products">
             <h2>Nouveaux Produits</h2>
-
             <?php foreach ($latestProducts as $product): ?>
                 <div class="product">
                     <img src="../../images/<?php echo $product['url_img']; ?>" alt="<?php echo $product['name']; ?>">
                     <h3><?php echo $product['name']; ?></h3>
                     <p>Prix : $<?php echo $product['price']; ?></p>
-                    <!-- Remplacez le commentaire par la description réelle -->
                     <p>Description : <?php echo $product['description']; ?></p>
                     <form method="post" action="home.php">
                         <input type="hidden" name="product_id" value="<?php echo $product['id']; ?>">
@@ -138,10 +129,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_to_cart'])) {
             <?php endforeach; ?>
         </div>
     </main>
-
     <footer>
         <p>&copy; <?php echo date("Y"); ?> Teccart Wear. Tous droits réservés.</p>
     </footer>
 </body>
-
 </html>
